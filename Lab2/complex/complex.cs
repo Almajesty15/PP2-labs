020b3eb1eaaeb2ba16f253418ca98826df82b69a
Almajesty15 <Almaz.Oral2000@gmail.com>
2018-02-06 11:40:05 +0600

    aa

---
+++ Lab2/complex/complex.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace complex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string c = s.Split(' ')[0];
+            string d = s.Split(' ')[1];
+            int a_ = int.Parse(c.Split('/')[0]);
+            int b_ = int.Parse(c.Split('/')[1]);
+            int c_ = int.Parse(d.Split('/')[0]);
+            int d_ = int.Parse(d.Split('/')[1]);
+            complex a = new complex(a_, b_);
+            complex b = new complex(c_, d_);
+            complex sum = a + b;
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
