212069649acbef9a9b1e789e41997a5769a15752
Almajesty15 <Almaz.Oral2000@gmail.com>
2018-02-07 11:36:38 +0600

    a

---
+++ Lab1/Student/program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circle
+{
+    public class Student
+    {
+        public string Surname, name;
+        public double gpa;
+        public int age;
+        public int height;
+
+        public Student()
+        {
+            Surname = "Oral";
+            name = "Almas";
+            age = 18;
+            height = 178;
+            gpa = 3;
+        }
+        public Student(string Surname, string name, int age, int height, double gpa)
+        {
+            this.Surname = Surname;
+            this.name = name;
+            this.age = age;
+            this.height = height;
+            this.gpa = gpa;
+        }
+        public static void getgpa(double gpa)
+        {
+            Console.WriteLine(gpa);
+        }
+
+        public override string ToString()
+        {
+            return Surname + ' ' + name + ' ' + age + ' ' + height + ' ' + gpa;
+        }
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Surname, name;
+            int age, height;
+            double gpa;
+            Surname = Console.ReadLine();
+            name = Console.ReadLine();
+            age = int.Parse(Console.ReadLine());
+            height = int.Parse(Console.ReadLine());
+            gpa = double.Parse(Console.ReadLine());
+
+            Student a = new Student(Surname, name, age, height, gpa);
+            Student.getgpa(a.gpa);
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
