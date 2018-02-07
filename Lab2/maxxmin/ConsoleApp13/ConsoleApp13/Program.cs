416eb54136beaa180f9c73e41dce9fbe4005a531
Almajesty15 <Almaz.Oral2000@gmail.com>
2018-02-07 11:47:09 +0600

    .

---
+++ Lab2/maxxmin/ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = File.ReadAllText(@"C:\Users\User\A.txt");
+            string[] line1 = line.Split(' ');
+            int maxx = -123123132;
+            int minn = 123123123;
+
+            foreach (string s in line1)
+            {
+                maxx = Math.Max(maxx, int.Parse(s));
+                minn = Math.Min(minn, int.Parse(s));
+            }
+
+            StreamWriter sw = new StreamWriter(@"C:\Users\User\A");
+            sw.WriteLine("max is " + maxx);
+            sw.WriteLine("min is " + minn);
+            sw.Close();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
