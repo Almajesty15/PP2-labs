4a020a670cdaafbe8ebe7ef36b8e865f67e6822d
Almajesty15 <Almaz.Oral2000@gmail.com>
2018-03-03 11:31:40 +0600

    a

---
+++ midterm/Problem1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace _1_problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n=int.Parse(Console.ReadLine());
+            
+            double a = Math.Pow(2,n);
+            Console.Write(a);
+        }
+    }
+}
---
+++ midterm/Problem2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input.txt array(a,b,c,...) second minimum in array-?    write to output.txt
+                  
+           // @c:\Users\user\A.txt
+}
+
+        private static void StreamReader(object A, object txt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
